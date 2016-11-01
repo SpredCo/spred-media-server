5b8344b7ba172213575bc58b1cd16b8a295bc5e6
Guillaume Galas <guillaume.galas@epitech.eu>
2016-11-01 17:42:00 +0100

    Serveur code useless commented

--- src/server.js
+++ src/server.js
@@ -20,104 +20,104 @@ var CharpyServer = require('./charpy-server');
 
 CharpyServer.start();
 
-/*
- * Definition of global variables.
- */
-var idCounter = 0;
-var candidatesQueue = {};
-var kurentoClient = null;
-var presenter = null;
-var viewers = [];
-var noPresenterMessage = 'No active presenter. Try again later...';
-
-function startViewer(session, ws, sdpOffer, callback) {
-	clearCandidatesQueue(session.id);
-
-	if (presenter === null) {
-		stop(session.id);
-		return callback(noPresenterMessage);
-	}
-
-	presenter.pipeline.create('WebRtcEndpoint', function(error, webRtcEndpoint) {
-		if (error) {
-			stop(session.id);
-			return callback(error);
-		}
-		viewers[session.id] = {
-			"webRtcEndpoint": webRtcEndpoint,
-			"ws": session
-		}
-
-		if (presenter === null) {
-			stop(session.id);
-			return callback(noPresenterMessage);
-		}
-
-		if (candidatesQueue[session.id]) {
-			while (candidatesQueue[session.id].length) {
-				var candidate = candidatesQueue[session.id].shift();
-				webRtcEndpoint.addIceCandidate(candidate);
-			}
-		}
-
-		webRtcEndpoint.on('OnIceCandidate', function(event) {
-			var candidate = kurento.getComplexType('IceCandidate')(event.candidate);
-			session.emit('iceCandidate', {
-				candidate: candidate
-			});
-		});
-
-		webRtcEndpoint.processOffer(sdpOffer, function(error, sdpAnswer) {
-			if (error) {
-				stop(session.id);
-				return callback(error);
-			}
-			if (presenter === null) {
-				stop(session.id);
-				return callback(noPresenterMessage);
-			}
-
-			presenter.webRtcEndpoint.connect(webRtcEndpoint, function(error) {
-				if (error) {
-					stop(session.id);
-					return callback(error);
-				}
-				if (presenter === null) {
-					stop(session.id);
-					return callback(noPresenterMessage);
-				}
-				callback(null, sdpAnswer);
-				webRtcEndpoint.gatherCandidates(function(error) {
-					if (error) {
-						stop(session.id);
-						return callback(error);
-					}
-				});
-			});
-		});
-	});
-}
-
-function clearCandidatesQueue(sessionId) {
-	if (candidatesQueue[sessionId]) {
-		delete candidatesQueue[sessionId];
-	}
-}
-
-function onIceCandidate(sessionId, _candidate) {
-	var candidate = kurento.getComplexType('IceCandidate')(_candidate);
-
-	if (presenter && presenter.id === sessionId && presenter.webRtcEndpoint) {
-		console.info('Sending presenter candidate');
-		presenter.webRtcEndpoint.addIceCandidate(candidate);
-	} else if (viewers[sessionId] && viewers[sessionId].webRtcEndpoint) {
-		console.info('Sending viewer candidate');
-		viewers[sessionId].webRtcEndpoint.addIceCandidate(candidate);
-	} else {
-		console.info('Queueing candidate');
-		if (!candidatesQueue[sessionId]) {
-			candidatesQueue[sessionId] = [];
-		}
-		candidatesQueue[sessionId].push(candidate);
-	}
-}
+// /*
+//  * Definition of global variables.
+//  */
+// var idCounter = 0;
+// var candidatesQueue = {};
+// var kurentoClient = null;
+// var presenter = null;
+// var viewers = [];
+// var noPresenterMessage = 'No active presenter. Try again later...';
+//
+// function startViewer(session, ws, sdpOffer, callback) {
+// 	clearCandidatesQueue(session.id);
+//
+// 	if (presenter === null) {
+// 		stop(session.id);
+// 		return callback(noPresenterMessage);
+// 	}
+//
+// 	presenter.pipeline.create('WebRtcEndpoint', function(error, webRtcEndpoint) {
+// 		if (error) {
+// 			stop(session.id);
+// 			return callback(error);
+// 		}
+// 		viewers[session.id] = {
+// 			"webRtcEndpoint": webRtcEndpoint,
+// 			"ws": session
+// 		}
+//
+// 		if (presenter === null) {
+// 			stop(session.id);
+// 			return callback(noPresenterMessage);
+// 		}
+//
+// 		if (candidatesQueue[session.id]) {
+// 			while (candidatesQueue[session.id].length) {
+// 				var candidate = candidatesQueue[session.id].shift();
+// 				webRtcEndpoint.addIceCandidate(candidate);
+// 			}
+// 		}
+//
+// 		webRtcEndpoint.on('OnIceCandidate', function(event) {
+// 			var candidate = kurento.getComplexType('IceCandidate')(event.candidate);
+// 			session.emit('iceCandidate', {
+// 				candidate: candidate
+// 			});
+// 		});
+//
+// 		webRtcEndpoint.processOffer(sdpOffer, function(error, sdpAnswer) {
+// 			if (error) {
+// 				stop(session.id);
+// 				return callback(error);
+// 			}
+// 			if (presenter === null) {
+// 				stop(session.id);
+// 				return callback(noPresenterMessage);
+// 			}
+//
+// 			presenter.webRtcEndpoint.connect(webRtcEndpoint, function(error) {
+// 				if (error) {
+// 					stop(session.id);
+// 					return callback(error);
+// 				}
+// 				if (presenter === null) {
+// 					stop(session.id);
+// 					return callback(noPresenterMessage);
+// 				}
+// 				callback(null, sdpAnswer);
+// 				webRtcEndpoint.gatherCandidates(function(error) {
+// 					if (error) {
+// 						stop(session.id);
+// 						return callback(error);
+// 					}
+// 				});
+// 			});
+// 		});
+// 	});
+// }
+//
+// function clearCandidatesQueue(sessionId) {
+// 	if (candidatesQueue[sessionId]) {
+// 		delete candidatesQueue[sessionId];
+// 	}
+// }
+//
+// function onIceCandidate(sessionId, _candidate) {
+// 	var candidate = kurento.getComplexType('IceCandidate')(_candidate);
+//
+// 	if (presenter && presenter.id === sessionId && presenter.webRtcEndpoint) {
+// 		console.info('Sending presenter candidate');
+// 		presenter.webRtcEndpoint.addIceCandidate(candidate);
+// 	} else if (viewers[sessionId] && viewers[sessionId].webRtcEndpoint) {
+// 		console.info('Sending viewer candidate');
+// 		viewers[sessionId].webRtcEndpoint.addIceCandidate(candidate);
+// 	} else {
+// 		console.info('Queueing candidate');
+// 		if (!candidatesQueue[sessionId]) {
+// 			candidatesQueue[sessionId] = [];
+// 		}
+// 		candidatesQueue[sessionId].push(candidate);
+// 	}
+// }
