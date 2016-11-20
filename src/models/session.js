a34fb83e0264504bbbda893ad7b7aff608124c6a
Guillaume Galas <guillaume.galas@epitech.eu>
2016-11-20 16:52:32 +0100

    session now integrate pipeline and webrtcsocket

--- src/models/session.js
+++ src/models/session.js
@@ -1,10 +1,21 @@
 var Session = function(socket) {
 	this.id = socket.id;
 	this.socket = socket;
+
+	// User id used in the database
 	this.user = null;
+
+	// Spredcast stored in the database
 	this.spredCast = null;
-	this.savedIceCandidate = [];
 	this.castToken = null;
+
+	// We store every iceCandidate for later use
+	this.savedIceCandidate = [];
+
+	this.pipeline = null;
+	this.webRtcEndpoint = null;
+	this.sdpAnswer = null;
+	this.sdpOffer = null;
 
 	return this;
 }
@@ -14,5 +25,14 @@ Session.prototype.addIceCandidateToQueue = function(iceCandidate) {
 		this.savedIceCandidate.push(iceCandidate);
 	}
 };
+
+Session.prototype.close = function() {
+	if (this.pipeline) {
+		this.pipeline.release();
+	} else if (this.webRtcEndpoint) {
+		this.webRtcEndpoint.release();
+	}
+	console.info(`Session[${this.id}] with user[${this.user.pseudo}] now close.`);
+}
 
 module.exports = Session;
