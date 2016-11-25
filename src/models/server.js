67de87d5caeedee6bc662b0ae2bb55e03a88ddd5
Guillaume Galas <guillaume.galas@epitech.eu>
2016-11-25 15:50:47 +0100

    https removed

--- src/models/server.js
+++ src/models/server.js
@@ -43,7 +43,7 @@ Server.prototype.start = function() {
 	var app = express();
 	var asUrl = url.parse(this.conf.as_uri);
 	var port = asUrl.port;
-	var httpsServer = http.createServer(this.options, app);
+	var httpsServer = http.createServer(app);
 	var wss = new io(httpsServer);
 
 	// TODO: SPREDCASTS IN THE SERVER -> Need to get them from DB with Spred is ready
