9f085351571688f064c26b89ccfbce3d6f048f3e
Guillaume Galas <guillaume.galas@epitech.eu>
2016-12-12 23:39:41 +0100

    question up and down working

--- src/models/question.js
+++ src/models/question.js
@@ -2,8 +2,7 @@ var Question = function(id) {
 	this.id = id;
 	this.text = null;
 	this.sender = null;
-	this.upVote = 0;
-	this.downVote = 0;
+	this.nbVote = 0;
 	this.date = new Date();
 
 	return this;
