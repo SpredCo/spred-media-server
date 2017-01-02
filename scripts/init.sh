a798c1cb4422f9c7620a85af8aa9b39e66db7850
Guillaume Galas <guillaume.galas@epitech.eu>
2017-01-02 16:06:49 +0100

    scripts init for dependencies

---
+++ scripts/init.sh
@@ -0,0 +1,2 @@
+rm -rf node_modules/websocket-stream/node_modules/ws && cp -r node_modules/ws node_modules/websocket-stream/node_modules/ws
+rm -rf node_modules/kurento-jsonrpc/node_modules/ws && cp -r node_modules/ws node_modules/kurento-jsonrpc/node_modules/ws
